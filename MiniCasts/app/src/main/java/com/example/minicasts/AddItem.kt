d06fbba56d2c07673b6021fcfe497ccc360a8885
Muditha Pradeeptha <it21235924@my.sliit.lk>
2024-01-18 13:10:49 +0530

    Dropdown function implementation

--- MiniCasts/app/src/main/java/com/example/minicasts/AddItem.kt
+++ MiniCasts/app/src/main/java/com/example/minicasts/AddItem.kt
@@ -2,10 +2,31 @@ package com.example.minicasts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 
 class AddItem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
+
+        val items = listOf("Hot Wheels","Takara Tomy","MiniAuto","Miniso","Majorette","MatchBox")
+
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete)
+
+        val adapter = ArrayAdapter(this,R.layout.activity_list_items,items)
+
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
+            adapterView, view, i, l ->
+
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this,"Item: $itemSelected",Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 }
--- MiniCasts/app/src/main/java/com/example/minicasts/UserDashboard.kt
+++ MiniCasts/app/src/main/java/com/example/minicasts/UserDashboard.kt
@@ -1,11 +1,23 @@
 package com.example.minicasts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.minicasts.databinding.ActivityUserDashboardBinding
 
 class UserDashboard : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_dashboard)
+        binding = ActivityUserDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addAModelCard.setOnClickListener {
+            val intent = Intent(this, AddItem::class.java)
+            startActivity(intent)
+        }
+
     }
 }
