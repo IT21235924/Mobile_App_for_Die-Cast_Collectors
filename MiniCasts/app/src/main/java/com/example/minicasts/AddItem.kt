748aae6fea36d52d01e7ca2e37862e46e67f884b
Muditha Pradeeptha <it21235924@my.sliit.lk>
2024-01-20 23:53:29 +0530

    small

--- MiniCasts/app/src/main/java/com/example/minicasts/AddItem.kt
+++ MiniCasts/app/src/main/java/com/example/minicasts/AddItem.kt
@@ -1,20 +1,30 @@
 package com.example.minicasts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
+import com.example.minicasts.databinding.ActivityAddItemBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AddItem : AppCompatActivity() {
 
-
-
+    private lateinit var binding: ActivityAddItemBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_item)
+
+        binding = ActivityAddItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         val items = listOf("Hot Wheels","Takara Tomy","MiniAuto","Miniso","Majorette","MatchBox")
 
@@ -29,6 +39,41 @@ class AddItem : AppCompatActivity() {
 
             val itemSelected = adapterView.getItemAtPosition(i)
             Toast.makeText(this,"Item: $itemSelected",Toast.LENGTH_SHORT).show()
+
+        }
+
+        binding.addItemBtn.setOnClickListener {
+
+            val name = binding.inputName.text.toString()
+            val vManu = binding.inputVManu.text.toString()
+            val year = binding.inputYear.text.toString()
+            val color = binding.inputColor.text.toString()
+            val special = binding.inputSpecial.text.toString()
+
+            if (name.isNotEmpty() && vManu.isNotEmpty() && year.isNotEmpty() && color.isNotEmpty() && special.isNotEmpty()){
+
+                val userID = FirebaseAuth.getInstance().currentUser!!.uid
+
+                val userMap = hashMapOf(
+                    "Car_Name" to name,
+                    "Vehicle_Manufacturer" to vManu,
+                    "Year" to year,
+                    "Color" to color,
+                    "Special_Details" to special
+                )
+
+                db.collection("Cars").document(userID).set(userMap).addOnSuccessListener {
+                    Toast.makeText(this, "Successfully Added!", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, UserDashboard::class.java)
+                    startActivity(intent)
+
+                }.addOnFailureListener {
+                    Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Please fill all fields!", Toast.LENGTH_SHORT).show()
+            }
 
         }
 
